0375fea9f9e3522d1d37072925e35ac9f23250f9
seax999 <yhx15779081105@163.com>
2022-12-01 21:47:19 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 
 function App() {
+
+
+
+    const [count, setCount] = useState(1);
+
+    const getSquare = useCallback((value)=>{
+        /**do something*/
+        return value * value
+    },[])
+
+
   return (
     <div className="App">
-123
+        <h1>this is count: {count}</h1>
+        <button onClick={()=>{
+            setCount(count+1)
+        }}>+1</button>
+        <br/>
+        <p>get square: {getSquare(count)}</p>
     </div>
   );
 }
